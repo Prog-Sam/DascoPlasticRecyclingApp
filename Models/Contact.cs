49ef5924dc2b2de7003584e35b606e9cede3d224
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 15:19:21 +0800

    Added Contact Model

---
+++ Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace DascoPlasticRecyclingApp.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ContactTypeId { get; set; }
+        public string value { get; set; }
+    }
+}
