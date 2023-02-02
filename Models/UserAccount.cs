21343c30879408611bed641beb4316dec27fd7a2
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 15:19:37 +0800

    Added User Account Model

---
+++ Models/UserAccount.cs
@@ -0,0 +1,11 @@
+﻿namespace DascoPlasticRecyclingApp.Models
+{
+    public class UserAccount
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool Admin { get; set; }
+    }
+}
