c3224cf0ee86b6cf0682570f108c625e4159ffee
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 20:06:59 +0800

    Updated Contact Model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -3,7 +3,7 @@
     public class Contact
     {
         public int Id { get; set; }
-        public string value { get; set; }
+        public string Value { get; set; }
 
         public User User { get; set; }
         public ContactType ContactType { get; set; }
