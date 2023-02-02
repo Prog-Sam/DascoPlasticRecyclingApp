75596663d54ad52dbe911cb893574d0963fd19a7
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 20:07:28 +0800

    Added Db Context

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DascoPlasticRecyclingApp.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<UserAccount> UserAccount { get; set; }
+        public DbSet<ContactType> ContactType { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+        public DbSet<PlasticType> PlasticType { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=127.0.0.1;database=dasco_db;uid=root;pwd=1234;");
+        }
+    }
+}
