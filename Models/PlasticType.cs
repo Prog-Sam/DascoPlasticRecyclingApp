3df396e54644c978acb7cfdf97ef3edac8667557
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 15:19:50 +0800

    Added Plastic Type Model

---
+++ Models/PlasticType.cs
@@ -0,0 +1,12 @@
+﻿namespace DascoPlasticRecyclingApp.Models
+{
+    public class PlasticType
+    {
+        public int Id { get; set; }
+        public int PlasticTypeId { get; set; }
+        public string? ImageLocation { get; set; }
+        public long Price { get; set; }
+        public long Qty { get; set; }
+
+    }
+}
