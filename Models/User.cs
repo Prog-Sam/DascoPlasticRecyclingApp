02a3922396d87518c5efcd41b26f4c06ead1998f
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-02 15:18:39 +0800

    Addeed User Model

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace DascoPlasticRecyclingApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string Lastname { get; set; }
+        public string Position { get; set; }
+    }
+}
