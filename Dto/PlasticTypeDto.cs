ca8bc522a16b745bf0f480e33ffda70a6b9968f1
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-03 01:43:25 +0800

    Added PlasticType Dto for data Control

---
+++ Dto/PlasticTypeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace DascoPlasticRecyclingApp.Dto
+{
+    public class PlasticTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? ImageLocation { get; set; }
+        public long Price { get; set; }
+        public long Qty { get; set; }
+    }
+}
