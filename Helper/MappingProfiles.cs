972cbe943289a12cfecf30ee2d6d5e1ffe8881ab
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-03 08:35:20 +0800

    Added AutoMapper

---
+++ Helper/MappingProfiles.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DascoPlasticRecyclingApp.Dto;
+using DascoPlasticRecyclingApp.Models;
+
+namespace DascoPlasticRecyclingApp.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<PlasticType, PlasticTypeDto>();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
+using DascoPlasticRecyclingApp.Interfaces;
+using DascoPlasticRecyclingApp.Models;
+using DascoPlasticRecyclingApp.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+//builder.Services.AddScoped<IPlasticTypeRepository, PlasticTypeRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>();
 
 var app = builder.Build();
 
