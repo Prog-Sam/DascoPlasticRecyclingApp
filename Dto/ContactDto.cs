50318127704d3e3e82295f5d9027ec83c5820c89
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-02-03 08:34:59 +0800

    Added Dto Group

---
+++ Dto/ContactDto.cs
@@ -0,0 +1,13 @@
+﻿using DascoPlasticRecyclingApp.Models;
+
+namespace DascoPlasticRecyclingApp.Dto
+{
+    public class ContactDto
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public User User { get; set; }
+        public ContactType ContactType { get; set; }
+    }
+}
---
+++ Dto/ContactTypeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace DascoPlasticRecyclingApp.Dto
+{
+    public class ContactTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
